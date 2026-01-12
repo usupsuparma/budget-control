1a588d5610f5c907503124ec16b8b8121813065b
Kusnandar2701 <kusnandar2701@gmail.com>
2026-01-12 20:51:36 +0700

    update nandar

---
+++ database/migrations/2026_01_11_231240_create_modul_menu_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modul_menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('modul_name');
+            $table->string('menu_name')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modul_menu');
+    }
+};

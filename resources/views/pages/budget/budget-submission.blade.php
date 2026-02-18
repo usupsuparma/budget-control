a982f7b061badcf11113db5c002f98fb98859713
UsupSuparma <usup.suparma7@gmail.com>
2026-02-18 23:03:01 +0700

    feat: Add new view for budget submission/movement management.

--- resources/views/pages/budget/budget-submission.blade.php
+++ resources/views/pages/budget/budget-submission.blade.php
@@ -138,7 +138,7 @@
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="budgetSubmissionModalLabel">Add Budget Submission</h5>
+                        <h5 class="modal-title" id="budgetSubmissionModalLabel">Add Budget Movement</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <form id="budgetSubmissionForm">
